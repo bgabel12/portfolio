9ec67bdf3a02f369eb4a817ed43661a7df9d5e47
bgabel12 <bgabel12@users.noreply.github.com>
2025-03-15 20:54:55 +0000

    deploy: 66a333bb8684592e477416aa86abef1f435393ea

---
+++ js/app.js
@@ -0,0 +1,35 @@
+﻿// app.js
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  var backToTopButton = document.getElementById("btn-back-to-top");
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    backToTopButton.style.display = "block";
+  } else {
+    backToTopButton.style.display = "none";
+  }
+}
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+// sets the given bootstrap theme
+function setTheme(theme) {
+  if (theme != 'light' && theme != 'dark') {
+    console.error("Invalid theme: " + theme);
+    theme = 'light';
+  }
+
+  document.documentElement.setAttribute("data-bs-theme", theme);
+}
+
+function showModal(name) {
+  var modal = new bootstrap.Modal(document.getElementById(name), {});
+  modal.show(); 
+}
