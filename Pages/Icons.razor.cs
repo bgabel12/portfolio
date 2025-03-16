97f1ffff5ecfeb0ce65e74dbd632946ba4566f1e
Icydeath <brent.gabel12@gmail.com>
2025-03-15 22:53:26 -0400

    Update UI and add icon functionality
    - Updated solution file for current Visual Studio version.
    - Enhanced `MainLayout.razor` with navigation buttons and theme toggle.
    - Updated section headers in `Home.razor` to include icons and added new skills.
    - Implemented functions in `app.js` for extracting and displaying icon classes.
    - Created new `Icons.razor` page for icon display with library selection.
    - Added `Icons.razor.cs` for JavaScript interop logic.
    - Introduced `Icons.razor.css` for styling icons and layout adjustments.

---
+++ Pages/Icons.razor.cs
@@ -0,0 +1,13 @@
+﻿// Icons.razor.cs
+using Microsoft.JSInterop;
+
+namespace BlazorWasmPortfolio.Pages
+{
+  public partial class Icons
+  {
+    public async Task GetIcons(string lib)
+    {
+      await jsRuntime.InvokeVoidAsync("displayIcons", lib);
+    }
+  }
+}
--- wwwroot/js/app.js
+++ wwwroot/js/app.js
@@ -33,3 +33,83 @@ function showModal(name) {
   var modal = new bootstrap.Modal(document.getElementById(name), {});
   modal.show(); 
 }
+
+// Methods for Icons page.
+function extractClasses(lib) {
+  const deviconClassRegex = /\.devicon-([a-z0-9-]+)-plain/g;
+  const fontawesomeClassRegex = /\.fa-([a-z0-9-]+)/g;
+  const bootstrapClassRegex = /\.bi-([a-z0-9-]+)/g;
+
+  var rx = deviconClassRegex;
+  if (lib == 'fa') {
+    rx = fontawesomeClassRegex;
+  }
+  else if (lib == 'bi') {
+    rx = bootstrapClassRegex;
+  }
+
+  // Iterate through all stylesheets
+  const classes = new Set();
+  for (let sheet of document.styleSheets) {
+    try {
+      for (let rule of sheet.cssRules) {
+        let matches = [...rule.cssText.matchAll(rx)];
+        matches.forEach(match => classes.add(match[0].replace('.', '')));
+      }
+    } catch (e) {
+      // Ignore cross-origin stylesheet errors
+    }
+  }
+
+  return Array.from(classes).sort();
+}
+
+function displayIcons(lib) {
+  document.getElementById("diContainer").innerHTML = "";
+  document.getElementById("faContainer").innerHTML = "";
+  document.getElementById("biContainer").innerHTML = "";
+  var containerName = lib + "Container";
+
+  const container = document.getElementById(containerName);
+  const iconClasses = extractClasses(lib);
+  iconClasses.forEach(iconClass => {
+    const iconBox = document.createElement("div");
+    iconBox.classList.add("icon-box");
+    iconBox.classList.add("text-bg-light");
+
+    const iconElement = document.createElement("i");
+    var cn = iconClass + " colored"; // fs-1
+    if (lib == "fa") {
+      cn = "fas " + iconClass + ""; // fs-1
+    }
+    else if (lib == "bi") {
+      cn = "bi " + iconClass + ""; // fs-1
+    }
+    iconElement.className = cn;
+
+    iconBox.onclick = function () {
+      IconClassToClipboard(cn);
+    };
+
+    const nameElement = document.createElement("div");
+    nameElement.classList.add("icon-name");
+    nameElement.textContent = iconClass.replace("devicon-", "").replace("-plain", "").replace("bi-", "").replace("fa-", "");
+
+    iconBox.appendChild(iconElement);
+    iconBox.appendChild(nameElement);
+    container.appendChild(iconBox);
+  });
+}
+
+function IconClassToClipboard(iconClass) {
+  if (iconClass) {
+    navigator.clipboard.writeText(iconClass);
+
+    /* Alert the copied text */
+    const toastDiv = document.querySelector('.toast');
+    //toastDiv.innerHTML = "" + iconClass + "<br/>" + "Copied to clipboard!";
+    toastDiv.innerHTML = "<div class='" + iconClass +" fs-4 d-inline me-2'></div> <div class='fs-4 d-inline'>Copied to clipboard!</div>"
+    const toast = new bootstrap.Toast(toastDiv);
+    toast.show();
+  }
+} 
