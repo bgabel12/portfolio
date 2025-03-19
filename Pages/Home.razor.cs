a37ae934150290a05c2beb24084f671dcc61c4a0
Icydeath <brent.gabel12@gmail.com>
2025-03-18 23:33:14 -0400

    Updated project to include skills.json for dynamic skill display. Added HttpClient injection in Home.razor Introduced Skill class and logic to fetch and categorize skills from the new JSON file.

--- Pages/Home.razor.cs
+++ Pages/Home.razor.cs
@@ -1,12 +1,25 @@
 ﻿// Home.razor.cs
 using Microsoft.JSInterop;
+using System.Collections.Generic;
+using System.Net.Http.Json;
+using static System.Net.WebRequestMethods;
 
 namespace BlazorWasmPortfolio.Pages
 {
   public partial class Home
   {
+    private List<string> SkillCategories = [];
+    private List<Skill> Skills = [];
     public string ImageTitle = string.Empty;
     public string ImageFileName = string.Empty;
+    
+    protected override async Task OnInitializedAsync()
+    {
+      Skills = await http.GetFromJsonAsync<List<Skill>>("content/skills.json") ?? [];
+      Skills.OrderBy(i => i.Name);
+      SkillCategories = Skills.DistinctBy(x => x.Category).Select(s => s.Category).ToList();
+    }
+
     /// <summary>
     /// Onclick event for images
     /// </summary>
---
+++ Skill.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorWasmPortfolio
+{
+  public class Skill
+  {
+    public string Category { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+  }
+}
